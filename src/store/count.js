ddc86aaa7f846162b6506237b35466d572b22996
Sergiu1-cyber <68728921+Sergiu1-cyber@users.noreply.github.com>
2023-01-13 12:30:55 +0200

    1 file modified

--- src/store/count.js
+++ src/store/count.js
@@ -2,5 +2,5 @@ import { create } from 'zustand';
 
 export const useCountStore = create((set) => ({
   counter: 0,
-  counterIncrement: () => set((state) => ({ counter: state.counter++ })),
+  counterIncrement: () => set((state) => ({ counter: state.counter + 1 })),
 }));
