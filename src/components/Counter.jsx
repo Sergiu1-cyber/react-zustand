bda2a5995bfe6438da4048fff35bc6de7cb1c50c
Sergiu1-cyber <68728921+Sergiu1-cyber@users.noreply.github.com>
2023-01-13 12:26:26 +0200

    2 files modified, 1 added

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import {useCountStore} from '../store/count'
+import React from 'react';
+import { useCountStore } from '../store/count';
 
 export default function Counter() {
-
   function CountRender() {
-    const count = useCountStore((state) => state.counter)
-    return <h3>Count:</h3>
+    const count = useCountStore((state) => state.counter);
+    return <h3>Count: {count}</h3>;
   }
 
   return (
     <div>
       <CountRender />
     </div>
-  )
+  );
 }
--- src/store/count.js
+++ src/store/count.js
@@ -1,7 +1,6 @@
-import {create} from 'zustand'
+import { create } from 'zustand';
 
-export const useCountStore = create(
-  () => ({
-    counter: 0
-  })
-)
+export const useCountStore = create((set) => ({
+  counter: 0,
+  counterIncrement: () => set((state) => ({ counter: state.counter++ })),
+}));
